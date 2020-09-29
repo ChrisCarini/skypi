5035925eb2bcddea4a985a5bef12aab4fe265ac5
ChrisCarini <6374067+ChrisCarini@users.noreply.github.com>
2020-09-28 22:36:13 -0700

    Adding sample local_variables.sh file.

---
+++ local_variables.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+##
+# Raspberry Pi - The below values are used when connecting from the local machine to the local Raspberry Pi running PiAware
+##
+PI_HOSTNAME=<ip_addr_here>
+PI_USER=<username_for_pi>
+PI_SSH_KEY=<path_to_ssh_key_for_user_above>
+
+##
+# External Host - The below values are used when connecting from the local machine to the External Host
+##
+EXTERNAL_HOST_HOSTNAME=<hostname_of_external_host>
+EXTERNAL_HOST_USERNAME=<username_to_connect_to_above_host>
+EXTERNAL_HOST_SSHKEY=<path_to_ssh_key_for_above_user_for_external_host>
+EXTERNAL_HOST_PATH=<full_path_on_remote_host>
